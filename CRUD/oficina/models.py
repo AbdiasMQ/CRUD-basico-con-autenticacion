857edf09a41798a75c67df0b93b6e01832cbbf04
Abdias <abdiasmamaniquiroga44@gmail.com>
2025-08-20 08:10:37 -0300

    agregue el archivo urls.py

--- CRUD/oficina/models.py
+++ CRUD/oficina/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class oficina(models.Model):
+    nombre = models.CharField(verbose_name="Nombre", max_length=100)
+    nombre_corto = models.CharField(verbose_name="Nombre Corto", max_length=50)
+
+    def __str__(self):
+        return f"{self.nombre} {self.nombre_corto}"
--- CRUD/persona/models.py
+++ CRUD/persona/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from oficina.models import oficina as Oficina
 
-# Create your models here.
+class persona(models.Model):
+    nombre = models.CharField(verbose_name="Nombre", max_length=100)
+    apellido = models.CharField(verbose_name="Apellido", max_length=100)
+    edad = models.IntegerField(verbose_name="Edad")
+    oficina = models.ForeignKey(Oficina,
+                                on_delete=models.CASCADE,
+                                related_name='personas',
+                                null=True, 
+                                blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} - {self.edad} - {self.oficina}"
