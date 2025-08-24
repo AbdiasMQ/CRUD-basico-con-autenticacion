bd37560ccd6c2b5a7a1489d6b776adb99fe5ae90
Abdias <abdiasmamaniquiroga44@gmail.com>
2025-08-24 19:13:35 -0300

    Agregue mas clases y urls en la app de oficina

--- CRUD/oficina/urls.py
+++ CRUD/oficina/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from . import views
+
 app_name = 'oficina'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('lista/', views.oficinaListView.as_view(), name='oficina_lista'),
+    path('buscar/', views.oficinaSearchView.as_view(), name='oficina_buscar'),
+    path('crear/', views.oficinaCreateView.as_view(), name='oficina_crear'),
+    path('editar/<int:pk>/', views.oficinaUpdateView.as_view(), name='oficina_editar'),
+    path('eliminar/<int:pk>/', views.oficinaDeleteView.as_view(), name='oficina_eliminar'),
+    path('detalle/<int:pk>/', views.oficinaDetailView.as_view(), name='oficina_detalle'),
 ]
--- CRUD/oficina/views.py
+++ CRUD/oficina/views.py
@@ -1,9 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hola Oficina!")
-
+from django.views import View
+from django.urls import reverse_lazy
+from .models import oficina
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
+class oficinaListView(LoginRequiredMixin, ListView):
+    model = oficina
+    template_name = 'oficina/oficina_lista.html'
+    context_object_name = 'oficinas'
 
+class oficinaDetailView(LoginRequiredMixin, DetailView):
+    model = oficina
+    template_name = 'oficina/oficina_detalle.html'
+    context_object_name = 'oficina'
+
+class oficinaCreateView(LoginRequiredMixin, CreateView):
+    model = oficina
+    template_name = 'oficina/oficina_crear.html'
+    fields = ['nombre', 'nombre_corto']
+    success_url = reverse_lazy('oficina:oficina_lista')
+
+class oficinaUpdateView(LoginRequiredMixin, UpdateView):
+    model = oficina
+    template_name = 'oficina/oficina_editar.html'
+    fields = ['nombre', 'nombre_corto']
+    success_url = reverse_lazy('oficina:oficina_lista')
+
+class oficinaDeleteView(LoginRequiredMixin, DeleteView):
+    model = oficina
+    template_name = 'oficina/oficina_eliminar.html'
+    success_url = reverse_lazy('oficina:oficina_lista')
+
+class oficinaSearchView(LoginRequiredMixin, ListView):
+    model = oficina
+    template_name = 'oficina/oficina_buscar.html'
+    context_object_name = 'oficinas'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return oficina.objects.filter(Q(nombre__icontains=query) | Q(nombre_corto__icontains=query))
+        return oficina.objects.none()
